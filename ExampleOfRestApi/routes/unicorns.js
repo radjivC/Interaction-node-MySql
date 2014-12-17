fee04e074f41d60daaf0c4ecd426fbd9dbada6e7
radjiv fender <carrereradjiv@yahoo.fr>
2014-12-17 17:49:16 +0100

    update routes file

--- ExampleOfRestApi/routes/unicorns.js
+++ ExampleOfRestApi/routes/unicorns.js
@@ -1,57 +1,85 @@
 var db          = require('mysql'); //This sets up the MySQL connection
-var db_pool     = db.createPool({
-  host        : 'localhost',
-  port        : '8889',
-  database    : 'node_js_sample',
-  user        : 'root',
-  password    : 'root'
-});
+var connection = mysql.createConnection(
+  {
+    host        : 'localhost',
+    port        : '8889',
+    database    : 'unicorn',
+    user        : 'root',
+    password    : 'root'
+  }
+);
 
 exports.findById = function(req, res) {
     var id = req.params.id;
     console.log('Retrieving unicorn: ' + id);
+    connection.connect();
 
-};
+    var queryString = 'SELECT'+id+'FROM unicorns';
+
+    connection.query(queryString, function(err, result) {
+        if (err) throw err;
+        unicorn = result;
+    });
+}
 
 exports.findAll = function(req, res) {
 
-};
+    var unicorns = [];
+    connection.connect();
+
+    var queryString = 'SELECT * FROM unicorns';
+
+    connection.query(queryString, function(err, rows, fields) {
+      if (err) throw err;
+
+      for (var i in rows) {
+        unicorns= rows[i];
+      }
+    });
+}
 
 exports.addUnicorn = function(req, res) {
     var unicorn = req.body;
+    console.log('add unicorn');
+    connection.connect();
+    var name=unicorn.name ;
+    var status=unicorn.status;
+    var birthday=unicorn.birthday; 
+    var queryString = 'INSERT'+name+','+status +','+birthday +','+'FROM unicorns';
+
+    connection.query(queryString, function(err, result) {
+        if (err) throw err;
+        console.log(result+ 'add OK');
+    });
 
 }
 
 exports.updateUnicorn = function(req, res) {
-    var id = req.params.id;
     var unicorn = req.body;
-    console.log('Updating unicorn: ' + id);
-    console.log(JSON.stringify(unicorn));
+    console.log('add unicorn');
+    connection.connect();
+    var name=unicorn.name ;
+    var status=unicorn.status;
+    var birthday=unicorn.birthday; 
+    var queryString = 'INSERT'+name+','+status +','+birthday +','+'FROM unicorns';
+
+    connection.query(queryString, function(err, result) {
+        if (err) throw err;
+        console.log(result+ 'insert OK');
+    });
 
 }
 
 exports.deleteUnicorn = function(req, res) {
-    var id = req.params.id;
-    console.log('Deleting unicorn: ' + id);
+    var id = req.id;
+    console.log('delete unicorn');
+    connection.connect();
+
+    var queryString = 'DELETE'+id+'FROM unicorns';
+
+    connection.query(queryString, function(err, result) {
+        if (err) throw err;
+        console.log(result+ 'delete OK');
+    });
 }
 
-/*--------------------------------------------------------------------------------------------------------------------*/
-// Populate database with sample data -- Only used once: the first time the application is started.
-// You'd typically not find this code in a real-life app, since the database would already exist.
-var populateDB = function() {
-
-    var unicorns = [
-    {
-      "name":"charlie",
-      "birthday": "02/10/2010",
-      "status":"sleepy"
-    },
-    {
-        "name":"sam",
-        "birthday": "02/10/2012",
-        "status":"exited"
-    }];
-
-
-
-};
